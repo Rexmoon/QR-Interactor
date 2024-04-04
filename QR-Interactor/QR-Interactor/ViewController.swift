39495b9873b4234d96d406dac8ba2046f187d466
rexmoon <lunajose6969@gmail.com>
2024-04-04 12:57:00 -0600

    feat: project updated

---
+++ QR-Interactor/QR-Interactor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QR-Interactor
+//
+//  Created by Jose Luna on 4/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
